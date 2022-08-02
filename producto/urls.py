03edfcff9d787f9dd516d376f616a3e55dcde065
Guadalupe Videla <sashavidelaa@gmail.com>
2022-08-02 20:01:44 -0300

    Entrega final finalizada

--- producto/urls.py
+++ producto/urls.py
@@ -5,7 +5,6 @@ from .views import mostrar_monitor, editar_monitor
 urlpatterns = [
     path('monitores/', views.ListaMonitores.as_view(), name = 'lista_monitores'),
     path('crear-monitor/', views.CrearMonitor.as_view(), name = 'crear_monitor'),
-    # path('editar-monitor/<int:pk>/', views.EditarMonitor.as_view(), name = 'editar_monitor'),
     path('editar-monitor/<int:id>/', editar_monitor, name = 'editar_monitor'),
     path('eliminar-monitor/<int:pk>/', views.EliminarMonitor.as_view(), name = 'eliminar_monitor'),
     path('mostrar-monitor/<int:id>/', mostrar_monitor, name = 'mostrar_monitor'),
